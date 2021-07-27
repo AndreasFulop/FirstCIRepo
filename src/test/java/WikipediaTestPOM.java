f14d1b0470f6b95c811240840c37b1a6080d6941
AndreasFulop <fulopand@freemail.hu>
2021-07-27 16:29:59 +0200

    more tests

--- src/test/java/WikipediaTestPOM.java
+++ src/test/java/WikipediaTestPOM.java
@@ -1,20 +1,29 @@
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 public class WikipediaTestPOM {
+
     @Test
     public void WikiTest() {
         Util.GetDriver().get("https://wikipedia.org/");
         MenuPage.ClickEnglishButton();
         String text = MainPage.GetLabel();
         Assertions.assertEquals("Welcome to Wikipedia,", text);
-
-        MainPage.clickWikipediaLink();
-        text = WikipediaPage.GetSubtitle();
-        Assertions.assertEquals("From Wikipedia, the free encyclopedia", text);
-
-        Util.CloseDriver();
     }
 
+    @Test
+    public void WikiTest2() {
+        Util.GetDriver().get("https://wikipedia.org/");
+        MenuPage.ClickEnglishButton();
+        MainPage.clickWikipediaLink();
+        String text = WikipediaPage.GetSubtitle();
+        Assertions.assertEquals("From Wikipedia, the free encyclopedia", text);
+    }
+
+    @AfterEach
+    public  void closing(){
+    Util.CloseDriver();
+    }
 
 }
